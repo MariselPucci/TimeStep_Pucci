edb717a6aba51070e74088bcc92b97ac8d8acf4e
Marisel Pucci <arq.marisel@gmail.com>
2022-07-03 17:41:48 -0300

    complete Readme, realice modificaciones para mejor funcionamiento del form

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Readme proyecto Time Step
+# Pagina web 
+## Proyecto: Instituto de Danzas Time Step
+Este es un proyecto realizado como trabajo final del Curso de Desarrollo Web de Coderhouse.
+Se implementó en un emprendimiento real (Instituto de Danzas Time Step) ya que carecían de página web y la idea fue promocionar y colaborar en el crecimiento del Instituto dentro del mercado a la vez que se implementaban los conocimientos adquiridos durante el curso.
+
+## Herramientas utilizadas:
+Html, Css, Sass, Bootstrap, librerías de Js. 
+Se trabajó en el desarrollo de la arquitectura del sitio, aplicacion de estilos utilizando colores ya utilizados por el Instituto en sus redes sociales, se trabajó en el diseño responsivo. Se cargó contenido real al sitio,se aplicaron técicas de SEO para mejorar el
+posicionamiento en motores de búsqueda, se creó un sitemap para buscadores y finalmente se cargó el contenido al servidor, con la configuración correcta para el funcionamiento del formulario de contacto y la redireccion a una casilla de mail para su prueba.
+
+### El trabajo fue realizado por: Marisel Pucci - 2022
--- form.php
+++ form.php
@@ -1,25 +1,25 @@
 <?php
 //Conecto el form con el server a través de los name 
-$nombre = $_POST["nombre"];
-$apellido = $_POST["apellido"];
-$email = $_POST["email"];
-$mensaje = $_POST["mensaje"];
+$nombre = $_GET['name'];
+$apellido = $_GET['lastname'];
+$email = $_GET['mail'];
+$mensaje = $_GET['mensaje'];
 
 //Como me va a llegar el cuerpo del mensaje 
-$mensaje = "El mensaje fue enviado por" . $nombre . ",\r\n";
-$mensaje = "Su apellido es:" . $apellido . "\r\n";
-$mensaje .= "Su email es:" . $email . "\r\n";
-$mensaje .= "Mensaje:" $_POST["mensaje"] . "\r\n";
-$mensaje .="Enviado el" .date("d/m/Y", time());
+$mensaje = "El mensaje fue enviado por: " . $nombre . ",\r\n";
+$mensaje .= "Su apellido es: " . $apellido . ",\r\n";
+$mensaje .= "Su email es: " . $email . ",\r\n";
+$mensaje .= "Mensaje: ". $_GET['mensaje'] . "\r\n";
+$mensaje .="Enviado el: " .date('d/m/Y', time());
 
-$destinatario = "arq.marisel@gmail.com";
-$asunto = "Este mail fue enviado desde la web de Time Step";
+$para = 'arq.marisel@gmail.com';
+$asunto = 'Este mail fue enviado desde la web de Time Step';
 
 //Funcion mail
 //Receptor del mail 
-mail($destinatario,$asunto, utf8_decode($mensaje). $header);
+mail($para, $asunto, utf8_decode($mensaje));
 
 //Redireccion una vez enviado el mail
-header("Location:exito.html");
+header('Location:pages/exito.html');
 
 ?>
